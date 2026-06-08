85712839f63a12ffa354394d00cba251456c0a2e
ziyuan618 <ziyuan618@github.com>
2026-06-08 23:40:01 +0800

    ObjC build - bare clang, no Swift runtime

--- build.sh
+++ build.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 set -e
-echo "Build..."
+echo "=== ObjC Build ==="
 
-cat > main.swift << 'SWIFT'
-import UIKit
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-    func application(_ app: UIApplication, didFinishLaunchingWithOptions opts: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let vc = UIViewController()
-        vc.view.backgroundColor = UIColor(red:0.04,green:0.04,blue:0.10,alpha:1)
-        let l = UILabel(frame:CGRect(x:0,y:200,width:UIScreen.main.bounds.width,height:100))
-        l.text = "TXGJ PRO"
-        l.textColor = UIColor(red:0,green:0.94,blue:1,alpha:1)
-        l.font = UIFont.boldSystemFont(ofSize:24)
-        l.textAlignment = .center
-        vc.view.addSubview(l)
-        window!.rootViewController = vc
-        window!.makeKeyAndVisible()
-        return true
+cat > main.m << 'OBJC'
+#import <UIKit/UIKit.h>
+@interface App : UIResponder <UIApplicationDelegate>
+@property UIWindow *window;
+@end
+@implementation App
+- (BOOL)application:(UIApplication*)app didFinishLaunchingWithOptions:(NSDictionary*)opts {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    UIViewController *vc = [UIViewController new];
+    vc.view.backgroundColor = [UIColor colorWithRed:0.04 green:0.04 blue:0.10 alpha:1];
+    UILabel *l = [[UILabel alloc] initWithFrame:CGRectMake(0,200,[UIScreen mainScreen].bounds.size.width,100)];
+    l.text = @"TXGJ PRO";
+    l.textColor = [UIColor colorWithRed:0 green:0.94 blue:1 alpha:1];
+    l.font = [UIFont boldSystemFontOfSize:24];
+    l.textAlignment = NSTextAlignmentCenter;
+    [vc.view addSubview:l];
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+@end
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([App class]));
     }
 }
-UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(AppDelegate.self))
-SWIFT
+OBJC
 
 cat > Info.plist << 'PLIST'
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0"><dict>
-<key>CFBundleExecutable</key><string>Tencent</string>
-<key>CFBundleIdentifier</key><string>com.tencent.phonemanager</string>
+<key>CFBundleExecutable</key><string>TXGJ</string>
+<key>CFBundleIdentifier</key><string>com.txg.protect</string>
 <key>CFBundleName</key><string>TXGJ</string>
 <key>CFBundleDisplayName</key><string>TXGJ</string>
 <key>CFBundlePackageType</key><string>APPL</string>
@@ -40,31 +46,21 @@ cat > Info.plist << 'PLIST'
 </dict></plist>
 PLIST
 
-# Try simplest possible compile
 SDK=$(xcrun --sdk iphoneos --show-sdk-path)
-FW="$SDK/System/Library/Frameworks"
-SWLIB=$(xcrun --sdk iphoneos --show-sdk-platform-path)/Developer/usr/lib
+echo "SDK: $SDK"
 
-# Method: swiftc directly with all needed flags
-xcrun swiftc \
-  -sdk "$SDK" \
-  -target arm64-apple-ios14.0 \
-  -F "$FW" \
-  -I "$FW" \
-  -L "$SWLIB" \
-  -Xlinker -rpath -Xlinker @executable_path/Frameworks \
-  -Xlinker -rpath -Xlinker /usr/lib/swift \
-  -Xlinker -add_empty_section -Xlinker __TEXT -Xlinker __swift5_types \
+xcrun clang -arch arm64 \
+  -isysroot "$SDK" \
+  -mios-version-min=14.0 \
   -framework UIKit -framework Foundation -framework CoreGraphics \
-  -o Tencent \
-  main.swift \
-  2>&1
+  -fobjc-arc \
+  -o TXGJ \
+  main.m 2>&1
 
-echo "Compile OK: $(file Tencent)"
+echo "Compiled: $(file TXGJ)"
 
-# Package
 mkdir -p output/Payload/TXGJ.app
-cp Tencent output/Payload/TXGJ.app/
+cp TXGJ output/Payload/TXGJ.app/
 cp Info.plist output/Payload/TXGJ.app/
 echo "APPL????" > output/Payload/TXGJ.app/PkgInfo
 cd output && zip -r ../TXGJ.ipa Payload/ && cd ..
